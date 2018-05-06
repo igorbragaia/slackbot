b73777b8c1a639e8334cdc885072ba91a4cb7476
Igor Bragaia <igor.bragaia@gmail.com>
2018-05-05 23:31:05 -0300

    deploying api

---
+++ app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+from db.operations import get_offered_trainings
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    x = get_offered_trainings()
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
