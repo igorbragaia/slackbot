ba171ff379b7a00c2d41d7533c57b0403476cb79
Igor Bragaia <igor.bragaia@gmail.com>
2018-05-06 11:45:52 -0300

    rest api ok

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
-from db.operations import get_offered_trainings
+from db.operations import get_offered_trainings, get_requested_trainings
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    trainings = get_offered_trainings()
-    trainings = [[item.team, item.suggestion] for item in trainings]
-    return render_template('index.html', trainings=trainings)
+    offered_trainings = [[item.id_slack, item.team, item.suggestion] for item in get_offered_trainings()]
+    requested_trainings = [[item.id_slack, item.team, item.suggestion] for item in get_requested_trainings()]
+    return render_template('index.html', requested_trainings=requested_trainings, offered_trainings=offered_trainings)
 
 
 if __name__ == '__main__':
